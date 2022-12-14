4010a6dd82f168f93497a54f879595dcf42f65c0
KalebGordon <112070896+KalebGordon@users.noreply.github.com>
2022-12-14 05:44:54 -0500

    Delete count_query.sql

--- count_query.sql
+++
@@ -1,9 +0,0 @@
--- Number of employees by their most recent job title who are about to retire
-select count(emp_no), title
-into retiring_titles
-from unique_titles
-where title is not null
-group by (title)
-order by count desc;
-
-select * from retiring_titles;
